dd6fd55b3f4d4fbe471428074bc80e80ae375b51
jrdemadara <jrdemadara@protonmail.com>
2025-08-05 20:24:20 +0800

    fix secondary logo size for mobile

--- resources/js/components/AppLogoSecondary.vue
+++ resources/js/components/AppLogoSecondary.vue
@@ -3,5 +3,5 @@ import AppLogoSecondaryIcon from '@/components/AppLogoSecondaryIcon.vue';
 </script>
 
 <template>
-    <AppLogoSecondaryIcon class="w-52 bg-transparent text-black dark:text-zinc-50" />
+    <AppLogoSecondaryIcon class="w-44 bg-transparent text-black lg:w-52 dark:text-zinc-50" />
 </template>
